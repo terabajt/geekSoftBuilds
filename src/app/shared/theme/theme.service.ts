622a6fb26ef101ceaaa4a1a84c7c4b979f46a1b4
marcinludwig <3653161+und3fin3d@users.noreply.github.com>
2024-08-06 22:53:52 +0200

    feat: add switch for angular material theme

--- src/app/shared/theme/theme.service.ts
+++ src/app/shared/theme/theme.service.ts
@@ -13,12 +13,24 @@ export class ThemeService {
   public lightThemeEnabled = () => computed(() => this.theme() === 'light-theme');
 
   public toggleTheme() {
-    this.theme.update((theme) => theme === 'light-theme' ? 'dark-theme' : 'light-theme');
+    this.theme.update((theme) => {
+      this.setThemeInHtml(theme);
+      return theme === 'light-theme' ? 'dark-theme' : 'light-theme'
+    });
+
   }
 
   private getDefaultTheme() {
     const isDarkTheme = this.document.defaultView?.matchMedia && this.document.defaultView?.matchMedia('(prefers-color-scheme: dark)').matches;
 
+    this.setThemeInHtml(isDarkTheme ? 'dark-theme' : 'light-theme');
+
     return isDarkTheme ? 'dark-theme' : 'light-theme' as const;
   };
+
+  private setThemeInHtml(theme: 'dark-theme' | 'light-theme') {
+    this.document.body.classList.remove('light-theme');
+    this.document.body.classList.remove('dark-theme');
+    this.document.body.classList.add(theme === 'light-theme' ? 'dark-theme' : 'light-theme');
+  }
 }
